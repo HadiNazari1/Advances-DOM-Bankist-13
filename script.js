3d037030c3cad582a9eccb61238f4021d3c8275f
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-03-06 10:04:54 +0900

    Implement random color change on nav link click
    Added random color functionality to navigation links.

--- script.js
+++ script.js
@@ -50,6 +50,23 @@ btnScrollTo.addEventListener('click', function(e){
     
     
 })
+const randomInt=(min,max) => Math.floor(Math.random()*(max-min+1)+min);
+const randomColor=() => `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+document.querySelector('.nav__link').addEventListener('click', function(e){
+    this.style.backgroundColor= randomColor();
+    e.preventDefault();
+
+})
+document.querySelector('.nav__links').addEventListener('click', function(e){
+    this.style.backgroundColor=randomColor();
+    e.preventDefault();
+
+})
+document.querySelector('.nav').addEventListener('click', function(e){
+    this .style.backgroundColor=randomColor();
+
+})
+
 
 
 
