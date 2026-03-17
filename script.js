4af8871a72e0ec8840ec942d848a9867f1ef0312
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-03-17 15:30:31 +0900

    Refactor modal and cookie message functionality

--- script.js
+++ script.js
@@ -88,7 +88,14 @@ h1.addEventListener('click',function(e){
 })
 
 
+const tabs=document.querySelectorAll('.operations__tab');
+const tabsContainer= document.querySelector('.operations__tab-container');
+const tabsContent=document.querySelectorAll('.operations__content');
 
+tabsContainer.addEventListener('click', function(e){
+  const clicked=e.target.closerst('.operations__tab');
+   
+})
 
 
 
