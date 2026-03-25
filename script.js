2255c88a79904aff1ff6f836c2239f12c97e0d5b
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-03-25 11:13:24 +0900

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ window.addEventListener('scroll', function(){
   if (this.window.scrollY > initialCoords.top)nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 })
-
+// The Intersection Observer API
 const header1 = document.querySelector('.header');
 const navHeight= nav.getBoundingClientRect().height;
 
