378881cbf48f450c190b45aa5151483d2c5437f5
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2026-03-27 10:19:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -149,24 +149,4 @@ const headerObserver= new IntersectionObserver(stickyNav,{
 headerObserver.observe(header);
 
 
-//Reveal Sections
-
-const allSevtions=document.querySelectorAll('.section');
-
-const revealSection=function(entries,observer){
-  const [entry]=entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver=new IntersectionObserver(revealSection,{
-  root:null,
-  threshold:0.15,
-
-})
-
-allSevtions.forEach(function(section){
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
+;
